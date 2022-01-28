20487d9edd1d4d66cf10660c801fc15b23f4072b
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-01-28 21:18:28 +0300

    update database script

--- resources/db_library.sql
+++ resources/db_library.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA library;
 
 SET search_path = library;
 
-CREATE TABLE account
+CREATE TABLE library.user
 (
     id           SERIAL PRIMARY KEY,
     username     CHARACTER VARYING(64) UNIQUE NOT NULL,
@@ -11,7 +11,8 @@ CREATE TABLE account
     first_name   CHARACTER VARYING(64)        NOT NULL,
     last_name    CHARACTER VARYING(64)        NOT NULL,
     phone_number CHARACTER VARYING(13) UNIQUE NOT NULL,
-    email        CHARACTER VARYING(64) UNIQUE
+    email        CHARACTER VARYING(64) UNIQUE,
+    is_banned    BOOLEAN                      NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE author
@@ -27,31 +28,39 @@ CREATE TABLE genre
     name CHARACTER VARYING(64) NOT NULL
 );
 
+CREATE TABLE section
+(
+    id   SERIAL PRIMARY KEY,
+    name CHARACTER VARYING(64) NOT NULL
+);
+
 CREATE TABLE book
 (
     id               SERIAL PRIMARY KEY,
-    name             CHARACTER VARYING(64)          NOT NULL,
-    author_id        INTEGER REFERENCES author (id) NOT NULL,
-    genre_id         INTEGER REFERENCES genre (id)  NOT NULL,
-    publication_year NUMERIC(4)                     NOT NULL
+    name             CHARACTER VARYING(64) UNIQUE    NOT NULL,
+    author_id        INTEGER REFERENCES author (id)  NOT NULL,
+    genre_id         INTEGER REFERENCES genre (id)   NOT NULL,
+    section_id       INTEGER REFERENCES section (id) NOT NULL,
+    quantity         NUMERIC(2)                      NOT NULL,
+    publication_year NUMERIC(4)                      NOT NULL
 );
 
 CREATE TABLE order_card
 (
     id            SERIAL PRIMARY KEY,
-    book_id       INTEGER REFERENCES book (id)    NOT NULL,
-    reader_id     INTEGER REFERENCES account (id) NOT NULL,
-    rental_time   DATE                            NOT NULL,
-    rental_period INTERVAL
+    reader_id     INTEGER REFERENCES library.user (id) NOT NULL,
+    rental_time   TIMESTAMP                            NOT NULL,
+    rental_period TIMESTAMP                            NOT NULL
 );
 
-CREATE TABLE black_list
+CREATE TABLE order_book
 (
-    user_id INTEGER REFERENCES account (id)
+    order_id INTEGER REFERENCES order_card (id) NOT NULL,
+    book_id  INTEGER REFERENCES book (id)       NOT NULL
 );
 
 INSERT INTO author(first_name, last_name)
-VALUES ('Джоан','Роулинг'),
+VALUES ('Джоан', 'Роулинг'),
        ('Viktor', 'Pelevin'),
        ('Vasya', 'Misha');
 
@@ -61,61 +70,79 @@ VALUES ('Fantasy'),
        ('Action'),
        ('Roman');
 
-INSERT INTO book (name, author_id, genre_id, publication_year) (
+INSERT INTO section(name)
+VALUES ('Бестселлеры'),
+       ('Научная литература'),
+       ('Художественная литература'),
+       ('Дестская литература');
+
+INSERT INTO book (name, author_id, genre_id, section_id, quantity, publication_year) (
     VALUES ('Generation P', (SELECT id FROM author WHERE last_name = 'Pelevin'),
-            (SELECT id FROM genre WHERE name = 'Action'), 2004),
+            (SELECT id FROM genre WHERE name = 'Action'),
+            (SELECT id FROM section WHERE name = 'Художественная литература'), 2, 2004),
            ('Wither', (SELECT id FROM author WHERE last_name = 'Misha'), (SELECT id FROM genre WHERE name = 'Roman'),
+            (SELECT id FROM section WHERE name = 'Художественная литература'), 1,
             1993)
 );
 
-INSERT INTO account(username, password, role, first_name, last_name, phone_number, email)
-VALUES ('login', 'password', 'reader', 'Misha', 'Pupkin', '+375445717918', 'misha@gamil.com'),
-       ('login1', 'password1', 'reader', 'Pasha', 'Aruchko', '+37544787911', 'pasha@gamil.com');
+INSERT INTO library.user(username, password, role, first_name, last_name, phone_number, email)
+VALUES ('admin', 'pass', 'admin', 'Dima', 'Basalay', '+37544874224', 'boss@gamil.com'),
+       ('login', 'password', 'user', 'Misha', 'Pupkin', '+375445717918', 'misha@gamil.com'),
+       ('login1', 'password1', 'user', 'Pasha', 'Aruchko', '+37544787911', 'pasha@gamil.com');
 
-INSERT INTO order_card(book_id, reader_id, rental_time, rental_period)
-VALUES ((SELECT id FROM book WHERE name = 'Generation P'), (SELECT id FROM account WHERE username = 'login'), '2022-01-07',
-        '30 day'),
-       ((SELECT id FROM book WHERE name = 'Wither'), (SELECT id FROM account WHERE username = 'login1'), '2022-01-06',
-        '10 day');
+INSERT INTO order_card(reader_id, rental_time, rental_period)
+VALUES ((SELECT id FROM library.user WHERE username = 'login1'), now(), '2022-01-30 09:10:47.638545'),
+       ((SELECT id FROM library.user WHERE username = 'login'), now(), '2022-02-27 09:10:47.638545');
 
-SELECT a.first_name AS first_name, a.last_name AS last_name
-FROM library.author a
-         JOIN library.book b ON a.id = b.author_id
-GROUP BY a.first_name, a.last_name, b.name
-HAVING b.name = 'Wither';
+INSERT INTO order_book (order_id, book_id)
+VALUES ((SELECT o.id
+         FROM order_card o
+                  JOIN library.user u ON o.reader_id = u.id
+         WHERE username = 'login'
+           AND rental_time = '2022-01-27 11:07:44.897268'),
+        (SELECT id FROM book WHERE name = 'Wither'OR id = 2 AND quantity != 0));
 
-SELECT b.name, b.publication_year, a.first_name, g.name
-FROM book b
-         JOIN author a ON b.author_id = a.id
-         JOIN genre g ON b.genre_id = g.id
-GROUP BY b.name, b.publication_year, a.first_name, g.name;
+UPDATE library.user
+SET is_banned = FALSE
+WHERE username = 'login';
 
-SELECT name,
-       u.first_name AS author_name,
-       u.last_name  AS author_surname,
-       a.first_name AS reader_name,
-       a.last_name  AS reader_surname,
-       phone_number,
-       email,
-       rental_time,
-       rental_period
-FROM library.order_card o
-         JOIN library.book b ON o.book_id = b.id
-         JOIN library.author u ON b.author_id = u.id
-         JOIN library.account a ON o.reader_id = a.id
-GROUP BY name, u.first_name, u.last_name, a.first_name, a.last_name, phone_number, email, rental_time, rental_period;
+UPDATE library.book SET quantity = 2 WHERE name = 'Wither';
 
-SELECT id,
+UPDATE library.book SET author_id = ?, genre_id = ?, section_id = ?, quantity = ?, publication_year = ? WHERE name = ?;
+
+/*DELETE
+FROM library.order_card oc
+USING library.order_book ob
+WHERE ob.order_id = oc.id AND oc.id = 2;*/
+
+SELECT order_id,
+       reader_id,
        username,
-       password,
        role,
-       first_name,
-       last_name,
+       u.first_name AS user_name,
+       u.last_name  AS user_surname,
+       book_id,
+       b.name       AS book_name,
+       a.first_name AS author_name,
+       a.last_name  AS author_surname,
+       g.name       AS genre_name,
+       s.name       AS section_name,
+       quantity,
+       publication_year,
+       rental_time,
+       rental_period,
        phone_number,
        email
-FROM library.account;
+FROM library.order_book ob
+         JOIN library.order_card o ON ob.order_id = o.id
+         JOIN library.book b ON ob.book_id = b.id
+         JOIN library.user u ON o.reader_id = u.id
+         JOIN library.author a ON b.author_id = a.id
+         JOIN library.genre g ON b.genre_id = g.id
+         JOIN library.section s ON b.section_id = s.id;
 
-INSERT INTO library.book (name, author_id, genre_id, publication_year) VALUES ((?), (SELECT id FROM author WHERE last_name = (?) AND first_name = (?)), (SELECT id FROM genre WHERE name = (?)), (?));
+drop table order_card cascade;
+drop table order_book cascade;
 
 drop schema library cascade;
 
