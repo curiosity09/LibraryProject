4d590a16f154fc41daa63ffd17c8a8de571708c4
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-01-28 21:16:39 +0300

    add new entity Section

---
+++ src/main/by/library/entity/Section.java
@@ -0,0 +1,52 @@
+package main.by.library.entity;
+
+public class Section {
+    private int id;
+    private String name;
+
+    public Section(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Section section = (Section) o;
+
+        if (id != section.id) return false;
+        return name != null ? name.equals(section.name) : section.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
